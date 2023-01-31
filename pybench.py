6c9dddd849d38329f815843fabe239f594f44dde
Lenoch <81lennoch@gmail.com>
2023-01-31 08:30:25 +0100

    Update pybench.py

--- pybench.py
+++ pybench.py
@@ -1,5 +1,7 @@
 import time
 
+print("\n--------------------------------\n\nStarting...\n")
+
 start_time = time.perf_counter()
 numerator = 4.0
 denominator = 1.0
@@ -14,5 +16,9 @@ end_time = time.perf_counter()
 execution_time = end_time - start_time
 score = (100 / execution_time)*700
 
-print("Time  : ", execution_time, "s")
-print("Score : ", score)
+print("Done!\n")
+print("Operations   : 100000000")
+print("Time         :", execution_time, "s")
+print("Operations/s :", 100000000/execution_time)
+print("Score        :", score)
+print("\n--------------------------------")
